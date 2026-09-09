7ba4032361055c2817b5a90b351f27cec1c3cb87
Ainur-1990 <Ainur-1990@users.noreply.github.com>
2026-09-09 19:50:56 +0300

    кадры: dr + манифесты

---
+++ media/turn/all.js
@@ -0,0 +1,6 @@
+/* манифест нарезки: обновляется tools/slice.mjs */
+window.__SEQ = window.__SEQ || {};
+window.__SEQ['free'] = { dir: 'media/turn/free/', count: 120, pad: 3, ext: '.webp' };
+window.__SEQ['l9'] = { dir: 'media/turn/l9/', count: 120, pad: 3, ext: '.webp' };
+window.__SEQ['zh'] = { dir: 'media/turn/zh/', count: 120, pad: 3, ext: '.webp' };
+window.__SEQ['dr'] = { dir: 'media/turn/dr/', count: 120, pad: 3, ext: '.webp' };
---
+++ media/turn/dr/manifest.js
@@ -0,0 +1,3 @@
+/* манифест нарезки: обновляется tools/slice.mjs */
+window.__SEQ = window.__SEQ || {};
+window.__SEQ['dr'] = { dir: 'media/turn/dr/', count: 120, pad: 3, ext: '.webp' };
