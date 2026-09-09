d64fbcf35195b56a599efacaeae6ac296c5d15cc
Ainur-1990 <Ainur-1990@users.noreply.github.com>
2026-09-09 19:49:17 +0300

    кадры: free + l9

---
+++ media/turn/free/manifest.js
@@ -0,0 +1,3 @@
+/* манифест нарезки: обновляется tools/slice.mjs */
+window.__SEQ = window.__SEQ || {};
+window.__SEQ['free'] = { dir: 'media/turn/free/', count: 120, pad: 3, ext: '.webp' };
---
+++ media/turn/l9/manifest.js
@@ -0,0 +1,3 @@
+/* манифест нарезки: обновляется tools/slice.mjs */
+window.__SEQ = window.__SEQ || {};
+window.__SEQ['l9'] = { dir: 'media/turn/l9/', count: 120, pad: 3, ext: '.webp' };
